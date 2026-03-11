1c3fffabc9ac5eea9f1668e63e999e1e3b1604f4
Charles <78650751+Charles-IX@users.noreply.github.com>
2026-03-11 13:38:17 +0800

    Initial Commit

---
+++ HomeworkToDo/HomeworkToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkToDoApp.swift
+//  HomeworkToDo
+//
+//  Created by Charles on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeworkToDoUITests/HomeworkToDoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeworkToDoUITestsLaunchTests.swift
+//  HomeworkToDoUITests
+//
+//  Created by Charles on 2026/3/11.
+//
+
+import XCTest
+
+final class HomeworkToDoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
